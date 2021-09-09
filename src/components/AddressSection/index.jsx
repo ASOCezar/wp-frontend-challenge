a1ae0bad1f3c8fa32c5e6877b4c00e13905a3996
ASOCezar <aso.cezar00@gmail.com>
2021-09-08 23:18:00 -0300

    Address Section mobile and tablet

--- src/components/AddressSection/index.jsx
+++ src/components/AddressSection/index.jsx
@@ -3,14 +3,9 @@ import * as Styled from './styles';
 export const AddressSection = () => (
   <Styled.Container>
     <div className="addres-info">
-      <span>3562 Eastham Drive Culver City CA 90232</span>
-    </div>
-    <div className="tooltip-adress">
-      <div className="tooltip">
-        <h4>Jam City HQ</h4>
-        <p>3562 Eastham Drive Culver City CA 90232</p>
-        <a href="#">View on Google Maps</a>
-      </div>
+      <span>
+        3562 Eastham Drive Culver City <br /> CA 90232
+      </span>
     </div>
   </Styled.Container>
 );
--- src/components/AddressSection/styles.js
+++ src/components/AddressSection/styles.js
@@ -5,5 +5,27 @@ import background from '../../assets/footer-images/footerBg.png';
 export const Container = styled.div`
   ${({ theme }) => css`
     background-image: url(${background});
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    padding-inline: 49px;
+    padding-block: 30px;
+    width: 100%;
+    height: 312px;
+    margin-top: 270px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+
+    .addres-info {
+      max-width: 280px;
+      color: ${theme.colors.white};
+      font-size: ${theme.font.sizes.xxlarge};
+      font-weight: ${theme.font.weight.black};
+      line-height: 34px;
+      letter-spacing: -0.9px;
+      text-align: center;
+    }
   `}
 `;
